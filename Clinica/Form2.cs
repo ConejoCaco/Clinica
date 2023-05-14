c2ce5ad36bbd5984ccd081886462a0032bd95d2a
Conejo Caco <huanca1992mamani@gmail.com>
2023-05-14 19:47:57 -0400

    fm1

--- Clinica/Form2.cs
+++ Clinica/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +14,12 @@ namespace Clinica
     public partial class Form2 : Form
     {
         Controlador ctr = new Controlador();
+        
         public Form2()
         {
             InitializeComponent();
         }
+        
 
         private void fm2button1_Click(object sender, EventArgs e)
         {
